9a2cb934c282b03c94d363512daeb87eabba0de9
Johnyzigos <142905030+Johnyzigos@users.noreply.github.com>
2024-02-11 00:21:31 +0200

    Add files via upload

---
+++ World happiness data analysis.R
@@ -0,0 +1,84 @@
+install.packages("tidyverse")
+library(tidyverse)
+# Load required library for plotting
+library(ggplot2)
+
+# Load the World Happiness Report dataset
+happiness_data <- read.csv("C:/Users/johny/Documents/Προγραμματισμός/project ideas/happiness report/happiness_report.csv", header = TRUE, sep = ",")
+
+#Check the class of data
+class(happiness_data)
+
+# View the first few rows of the dataset
+head(happiness_data, 8)
+
+# Summary statistics of the dataset
+summary(happiness_data)
+
+# Create a bar plot
+ggplot(happiness_data, aes(x = Country.or.region, y = Score)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  labs(title = "life expectancy by Country", x = "Country", y = "Score") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+
+# Fit a linear regression model
+model <- lm(`Score` ~ `Freedom.to.make.life.choices`, data = happiness_data)
+
+# Summary of the linear regression model
+summary(model)
+
+# Plot the linear regression line
+ggplot(happiness_data, aes(x = `Freedom.to.make.life.choices`, y = `Score`)) +
+  geom_point(color = "green") +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  labs(title = "Linear Regression: Happiness Score vs Freedom",
+       x = "Freedom", y = "Happiness Score")
+
+# Fit a linear regression model
+model_health_score <- lm(Score ~ `Healthy.life.expectancy`, data = happiness_data)
+
+# Summary of the linear regression model
+summary(model_health_score)
+
+# Plot the linear regression line
+ggplot(happiness_data, aes(x = `Healthy.life.expectancy`, y = Score)) +
+  geom_point(color = "blue") +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  labs(title = "Linear Regression: Happiness Score vs Healthy Life Expectancy",
+       x = "Healthy Life Expectancy", y = "Happiness Score")
+
+#The world happiness!
+mean(happiness_data$Score)
+var(happiness_data$Score)
+
+library(dplyr)
+
+# Filter the dataset for the top 10 ranked countries
+top_10_countries <- happiness_data %>%
+  top_n(10, wt = Score)
+
+# Create a scatter plot for the top 10 ranked countries
+ggplot(top_10_countries, aes(x = `Social.support`, y = Score, color = Country.or.region)) +
+  geom_point() +
+  labs(title = "Scatter Plot: Social Support vs Happiness Score (Top 10 Countries)",
+       x = "Social Support",
+       y = "Happiness Score",
+       color = "Country or Region") +
+  theme_minimal()
+
+library(corrplot)
+
+# Calculate the correlation matrix
+correlation_matrix <- cor(happiness_data[, c("Score", "Social.support", "Freedom.to.make.life.choices", "Healthy.life.expectancy", "Generosity", "Perceptions.of.corruption")])
+
+# Visualize the correlation matrix using corrplot
+corrplot(correlation_matrix, method = "circle", type = "upper", tl.pos = "lt", tl.col = "black", tl.cex = 0.8)
+
+# Create a new variable high and low social support
+happiness_data <- mutate(happiness_data, Social_Support_Group = ifelse(`Social.support` >= mean(`Social.support`, na.rm = TRUE), "High", "Low"))
+
+# Perform a t-test to compare the average happiness scores between groups
+t_test_result <- t.test(happiness_data$Score ~ happiness_data$Social_Support_Group)
+t_test_result
+
+
